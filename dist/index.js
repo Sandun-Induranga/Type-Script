f7cc52084174daf5dc18946c9b8b2ce22946f849
Sandun Induranga <sandutharu40@gmail.com>
2023-03-04 14:21:47 +0530

    TypeScript Functions

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+//  **********************************        Data Types        ****************************************
+// Numbers
+let age = 10;
+// String
+let firstName = "Dasun";
+// Array
+let numbers = [10, 20, 30, 40, 50];
+let strings = ["A", "B", "C", "D", "E"];
+// Tuples
+let person = [1, "Dasun"];
+//  *************************************        Functions        ******************************************
+function calculate() {
+    return "";
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,37 @@
-let x = 10+10;
-console.log(x);
+//  **********************************        Data Types        ****************************************
+
+
+// Numbers
+
+let age: number = 10;
+
+
+// String
+
+let firstName: string = "Dasun";
+
+
+// Array
+
+let numbers: number[] = [10, 20, 30, 40, 50];
+let strings: string[] = ["A","B","C","D","E"];
+
+// Tuples
+let person: [number, string] = [1, "Dasun"];
+
+
+
+
+//  *************************************        Functions        ******************************************
+
+
+function calculate() {
+    return "";
+}
+
+
+// Function With Type Safe
+
+function calculateAverageMark(subjectCount:number, totalMarks:number): number {
+    return 0;
+}
